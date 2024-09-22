77990150fb4e76e3e1f84f3fb41f0821ab1b18dc
Ag1102 <146095414+Ag1102@users.noreply.github.com>
2024-09-22 15:00:05 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+document.addEventListener("DOMContentLoaded", function() {
+    // Selecciona el botón por su ID
+    var boton = document.getElementById("miBoton");
+
+    // Agrega un evento de clic al botón
+    boton.addEventListener("click", function() {
+        alert("¡Has hecho clic en el botón!");
+    });
+});
+
